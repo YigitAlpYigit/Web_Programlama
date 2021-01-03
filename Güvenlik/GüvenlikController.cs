fdb9675e49e904166e46b079d75f64eb4a38357d
YigitAlpYigit <47749435+YigitAlpYigit@users.noreply.github.com>
2021-01-03 22:38:45 +0300

    Update GüvenlikController.cs

--- Güvenlik/GüvenlikController.cs
+++ Güvenlik/GüvenlikController.cs
@@ -11,6 +11,8 @@ namespace WebMvcStok.Controllers
     public class GuvenlikController : Controller
     {
         HayvanSahiplenmeEntities db = new HayvanSahiplenmeEntities();
+
+        [HttpGet]
         public ActionResult GirisYap()
         {
             return View();
@@ -27,13 +29,74 @@ namespace WebMvcStok.Controllers
             }
             else
             {
+                ViewBag.LoginError = "Giriş Bilgileriniz Hatalıdır...";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult UyeGirisYap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UyeGirisYap(TBLALICI t)
+        {
+            var bilgiler = db.TBLALICI.FirstOrDefault(x => x.KULLANICIEPOSTA == t.KULLANICIEPOSTA && x.KULLANICISIFRE == t.KULLANICISIFRE);
+            if (bilgiler != null)
+            {
+                bilgiler.KULLANICIIP = t.KULLANICIIP;
+                db.SaveChanges();
+                return RedirectToAction("Index", "Anasayfa");
+            }
+            else
+            {
+                ViewBag.LoginError = "Giriş Bilgileriniz Hatalıdır...";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult YeniUye()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniUye(TBLALICI p1)
+        {
+            bool kontrol = true;
+
+            foreach (var yeniuye in db.TBLALICI)
+            {
+                if (yeniuye.KULLANICIEPOSTA == p1.KULLANICIEPOSTA)
+                {
+                    kontrol = false;
+                }
+            }
+
+            if (kontrol == true)
+            {
+                db.TBLALICI.Add(p1);
+                db.SaveChanges();
+                return RedirectToAction("UyeGirisYap", "Guvenlik");
+            }
+            else
+            {
+                ViewBag.LoginError = "E-Posta Kullanılmaktadır.";
                 return View();
             }
         }
         public ActionResult Logout()
-        {
+        { 
             FormsAuthentication.SignOut();
             return RedirectToAction("GirisYap", "Guvenlik");
+        }
+        public ActionResult LogoutUye()
+        { 
+            FormsAuthentication.SignOut();
+            return RedirectToAction("UyeGirisYap", "Guvenlik");
         }
     }
 }
