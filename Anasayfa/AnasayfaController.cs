14d166f4606256eadf705c831f319e7209dd5577
YigitAlpYigit <47749435+YigitAlpYigit@users.noreply.github.com>
2021-01-03 22:35:18 +0300

    Anasayfa

---
+++ Anasayfa/AnasayfaController.cs
@@ -0,0 +1,27 @@
+﻿using HayvanSahiplendirmeSitesi.Models.Entity;
+using HayvanSahiplendirmeSitesi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HayvanSahiplendirmeSitesi.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+        HayvanSahiplenmeEntities db = new HayvanSahiplenmeEntities();
+
+        public ActionResult Index()
+        {
+            var kategoriler = db.TBLKATEGORILER.ToList();
+            var hayvanlar = db.TBLHAYVAN.ToList();
+            var alicilar = db.TBLALICI.ToList();
+            ViewModel vm = new ViewModel();
+            vm.Hayvan = hayvanlar.ToList();
+            vm.Kategori = kategoriler.ToList();
+            vm.Alıcı = alicilar.ToList();
+            return View(vm);
+        }
+    }
+}
