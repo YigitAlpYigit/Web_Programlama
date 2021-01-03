567cdd20ef1eb91d5946ead814a8468d1b70160f
YigitAlpYigit <47749435+YigitAlpYigit@users.noreply.github.com>
2021-01-03 22:35:58 +0300

    Dil Değiştirme

---
+++ Language/LanguageController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HayvanSahiplendirmeSitesi.Controllers
+{
+    public class LanguageController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Change(String LanguageAbbrevation)
+        {
+            if(LanguageAbbrevation != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(LanguageAbbrevation);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(LanguageAbbrevation);
+            }
+
+            HttpCookie cookie = new HttpCookie("Language");
+            cookie.Value = LanguageAbbrevation;
+            Response.Cookies.Add(cookie);
+
+            return RedirectToAction("Index","Anasayfa");
+        }
+    }
+}
